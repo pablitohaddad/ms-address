eb689643e0952c0438ef91e633fb886da3f87933
pablitohaddad <pablohaddad73@gmail.com>
2024-02-27 13:14:14 -0400

    Initial commit

---
+++ src/main/java/io/github/pablitohaddad/msaddress/MsaddressApplication.java
@@ -0,0 +1,13 @@
+package io.github.pablitohaddad.msaddress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsaddressApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsaddressApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/pablitohaddad/msaddress/MsaddressApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.pablitohaddad.msaddress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsaddressApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
